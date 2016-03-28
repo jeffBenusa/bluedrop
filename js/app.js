dc520bc866dda86fe11d170a97540a4885dcc103
jeffBenusa <jeff.benusa@gmail.com>
2016-03-28 15:08:31 -0500

    added functionality for click and add markers

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 
+  var mouseLocation = [];
+
   L.mapbox.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
 
   var mymap = L.map('map').setView([51.505, -0.09], 10);
@@ -11,38 +13,66 @@ $( document ).ready(function() {
     id: 'mapbox.streets'
   }).addTo(mymap);
 
+
+  // London center marker
   var marker = L.marker([51.5, -0.09]).addTo(myLayer);
 
 
-  // Random marker below
-  console.log("Random location close to 51.50");
-  console.log(50.2+(Math.random() * 0.5) + 1);
-  var lat = (50.2+(Math.random() * 0.5) + 1);
-
-  console.log("Random location near -0.09");
-  console.log(.02+(Math.random() * 0.05) + .01);
-  var long = (-.39+(Math.random() * .75));
-
-  var marker = L.marker([lat,long]).addTo(myLayer);
-
+  // Adding random marker
   $("#random_container").click(function(){
     addnewMarker();
   });
 
-
   function addnewMarker(){
-      console.log("Random location close to 51.50");
-      console.log(50.2+(Math.random() * 0.5) + 1);
       var lat = (50.2+(Math.random() * 0.5) + 1);
-
-      console.log("Random location near -0.09");
-      console.log(.02+(Math.random() * 0.05) + .01);
       var long = (-.39+(Math.random() * .75));
-
       var marker = L.marker([lat,long]).addTo(myLayer);
-}
-});
+  }
 
-  // var mymap = L.map('map').setView([51.505, -0.09], 10);
-  // var myLayer = L.mapbox.featureLayer().addTo(mymap);
-  // var marker = L.marker([51.5, -0.09]).addTo(myLayer);
+
+
+  $('#map').on('load', function() {
+    $(".trigger").fadeOut(1000);
+    console.log("check");
+  });
+
+
+  // attaching function on map click
+  mymap.on('click', onMapClick);
+
+  // Script for adding marker on map click
+  function onMapClick(e) {
+
+    var geojsonFeature = {
+      "type": "Feature",
+          "properties": {},
+          "geometry": {
+              "type": "Point",
+              "coordinates": [e.latlng.lat, e.latlng.lng]
+            }
+      }
+
+    var cssIcon = L.divIcon({
+    // Specify a class name we can refer to in CSS.
+      className: 'css-icon',
+      // Set marker width and height
+      iconSize: [100, 100]
+    });
+
+    var marker;
+
+    L.geoJson(geojsonFeature, {
+    pointToLayer: function(feature, latlng){
+        marker = L.marker(e.latlng, {
+            icon: cssIcon,
+            title: "Resource Location",
+            alt: "Resource Location",
+            riseOnHover: true,
+            draggable: false,
+            opacity:.8,
+        }).bindPopup();
+        //marker.on("popupopen", onPopupOpen);
+        return marker;
+    }}).addTo(mymap);
+    }
+});
