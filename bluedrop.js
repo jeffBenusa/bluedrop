4e13109fd03c6ed17816ad00c01b779fd7224e2e
Jeff Benusa <jeff.benusa@gmail.com>
2016-04-29 15:10:52 -0500

    express to serve map

--- bluedrop.js
+++ bluedrop.js
@@ -1,41 +1,23 @@
-//Lets require/import the HTTP module
-var http = require('http');
-var WebSocketServer = require('ws').Server;
+'use strict';
 
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
-var wss = new WebSocketServer({
-  port: 3000
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+  console.log("hello world");
+  //__dirname : It will resolve to your project folder.
 });
 
-wss.on('connection',function (ws){
-  ws.on('message',function(message){
-    console.log('received: %s', message);
-    ws.send('from server -- ' + message);
-  });
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
 });
 
+app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
 
+app.listen(3000);
 
-// //Lets define a port we want to listen to
-// const PORT=3000;
-//
-// //We need a function which handles requests and send response
-// function handleRequest(request, response){
-//     response.end('It Works!! Path Hit: ' + request.url);
-// }
-//
-// //Create a server
-// var server = http.createServer(handleRequest);
-//
-// //Lets start our server
-// server.listen(PORT, function(){
-//     //Callback triggered when server is successfully listening. Hurray!
-//     console.log("Server listening on: http://45.55.237.122", PORT);
-// });
-//
-// var BluedropSocket = new WebSocket("ws:45.55.237.122:3000")
-//
-//
-// exampleSocket.onmessage = function (event) {
-//   console.log(event.data);
-// }
+console.log("Running at Port 3000");
